3acd6af5d0178c9c0a3a8ec031fd6794eebed0af
ivanfsilva <ivanfs27@gmail.com>
2020-04-20 18:53:22 -0300

    Criando múltiplos dicionarios

--- src/main/java/br/com/ivanfsilva/jogoforca/core/Dictionary.java
+++ src/main/java/br/com/ivanfsilva/jogoforca/core/Dictionary.java
@@ -7,46 +7,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Dictionary {
-
-    private static final String FILE_NAME = "dicionario.txt";
+public abstract class Dictionary {
 
     private static Dictionary instance;
 
-    private List<String> words = new ArrayList<>();
-
-    public Dictionary() {
-        load();
-    }
-
     public static Dictionary getInstance() {
         if(instance == null) {
-            instance = new Dictionary();
+            instance = new FileDictionary();
         }
         return instance;
     }
 
-    private void load() {
-        try (Scanner scanner =
-                     new Scanner(getClass()
-                             .getResourceAsStream("/" + FILE_NAME))) {
+    public abstract Word nextWord();
 
-            while (scanner.hasNextLine()) {
-                String word = scanner.nextLine().trim();
-                words.add(word);
-            }
-
-            if (words.size() == 0) {
-                throw new GameException("A lista de palavras não pode estar vazia");
-            }
-
-        }
-
-    }
-
-    public Word nextWord() {
-        int pos = RandomUtils.newRandomNumber(0, words.size());
-        return new Word(words.get(pos));
-    }
+    public abstract String getName();
 
 }
