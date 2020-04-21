5b746b87d6cffdfa76bbfd61dd68cef4ace8a9d7
ivanfsilva <ivanfs27@gmail.com>
2020-04-20 22:37:10 -0300

    Escolhendo o dicionário a ser usado

--- src/main/java/br/com/ivanfsilva/jogoforca/core/Dictionary.java
+++ src/main/java/br/com/ivanfsilva/jogoforca/core/Dictionary.java
@@ -3,6 +3,7 @@ package br.com.ivanfsilva.jogoforca.core;
 import br.com.ivanfsilva.jogoforca.game.GameException;
 import br.com.ivanfsilva.jogoforca.utils.RandomUtils;
 
+import java.lang.reflect.Constructor;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -13,7 +14,14 @@ public abstract class Dictionary {
 
     public static Dictionary getInstance() {
         if(instance == null) {
-            instance = new FileDictionary();
+            try {
+                String dictionaryClassName = Config.get("dictionaryClassName");
+                Class<?> clazz = Class.forName(dictionaryClassName);
+                Constructor<?> constructor = clazz.getConstructor();
+                instance = (Dictionary) constructor.newInstance();
+            } catch (Exception e) {
+                throw  new RuntimeException(e);
+            }
         }
         return instance;
     }
